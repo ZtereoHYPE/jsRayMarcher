7923144c84a7950bcf021292a5cf934a4b7cb887
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2021-08-16 01:04:17 +0200

    new: rotation and matrix stuff (very yay)

--- sketch_3d.js
+++ sketch_3d.js
@@ -19,7 +19,7 @@ let generatorArray = [
 		type: "plane",
 		normal: {
 			x: 0,
-			y: -1,
+			y: 1,
 			z: 0
 		},
 		point: {
@@ -53,15 +53,16 @@ function generateSpheres() {
 
 let canvas;
 let vectorArray = [];
-let fragmentSize = 4;
+let fragmentSize = 6;
 let playerLocation;
 let playerRotationY = 0;
-let lightVector
-let maxCircles
+let playerRotationX = 0;
+let lightVector;
+let maxCircles;
 function setup() {
 	playerLocation = createVector(0, 0, -100);
 	generateSpheres()
-	frameRate(10)
+	frameRate(10);
 	canvas = createCanvas(400, 400);
 	background(10);
 	lightVector = createVector(10 * Math.cos(frameCount), -10, -10)
@@ -84,7 +85,7 @@ function draw() {
 			let chosenSphere;
 
 			maxCircles = 0;
-			while (smallestSphereDistance > 0.01  && (currentRayLocation.z - playerLocation.z) < 300 && (currentRayLocation.x - playerLocation.x) < 300 && (currentRayLocation.y - playerLocation.y) < 300) {
+			while (smallestSphereDistance > 0.1 && currentRayLocation.dist(playerLocation) < 500) {
 				for (sphere of sphereArray) {
 					if (sphere.type === "sphere") {
 						let distance = Math.abs(currentRayLocation.dist(sphere.vect) - sphere.r)
@@ -92,7 +93,7 @@ function draw() {
 							smallestSphereDistance = distance;
 							chosenSphere = sphere;
 						};
-					} 
+					}
 					else if (sphere.type === "plane") {
 						let distance = Math.abs(p5.Vector.dot(sphere.normal, p5.Vector.sub(currentRayLocation, sphere.point)));
 						if (distance < smallestSphereDistance) {
@@ -104,9 +105,30 @@ function draw() {
 
 				// let surfaceData = getSurfaceDistance(currentRayLocation)
 				// chosenSphere = surfaceData[1]
+				
 				let correctVector = createVector(u, v, 1).normalize();
-				let rotatedVector = createVector(correctVector.x, correctVector.y).rotate(radians(playerRotationY))
-				currentRayLocation.add(createVector(rotatedVector.x * smallestSphereDistance, rotatedVector.y * smallestSphereDistance, correctVector.z * smallestSphereDistance))
+				let angle = radians(playerRotationY);
+
+				let rayVectorMatrix = [
+					[correctVector.x, correctVector.y, correctVector.z]
+				]
+				let YtransformationMatrix = [
+					[Math.cos(angle), 0, -Math.sin(angle)],
+					[0, 1, 0],
+					[Math.sin(angle), 0, Math.cos(angle)]
+				]
+
+				// console.log(YtransformationMatrix)
+				// let XtranformationMatrix = [
+				// 	[1, 0, 0],
+				// 	[0, Math.cos(playerRotationX), -Math.sin(playerRotationX)],
+				// 	[0, Math.sin(playerRotationX), Math.cos(playerRotationX)]
+				// ]
+
+				let matriceVector = multiplyMatrices(rayVectorMatrix, YtransformationMatrix);
+				
+				// matriceVector = multiplyMatrices(matriceVector, XtranformationMatrix);
+				currentRayLocation.add(createVector(matriceVector[0][0] * smallestSphereDistance, matriceVector[0][1] * smallestSphereDistance, matriceVector[0][2] * smallestSphereDistance));
 				maxCircles++
 			}
 
@@ -117,8 +139,8 @@ function draw() {
 				let r = chosenSphere.colour[0];
 				let g = chosenSphere.colour[1];
 				let b = chosenSphere.colour[2];
-				fill((normal.x*0.5 +0.5) * 255, (normal.y*0.5 +0.5) * 255, (normal.z*0.5 +0.5) * 255)
-				// fill(max(0.2 * r, brightness * r), max(0.2 * g, brightness * g), max(0.2 * b, brightness * b))
+				// fill((normal.x*0.5 +0.5) * 255, (normal.y*0.5 +0.5) * 255, (normal.z*0.5 +0.5) * 255)
+				fill(max(0.2 * r, brightness * r), max(0.2 * g, brightness * g), max(0.2 * b, brightness * b))
 				square(x, y, fragmentSize);
 			}
 		}
@@ -132,7 +154,6 @@ function movePlayer() {
 	if (keyIsDown(87)) {
 		playerLocation.z += 2
 	}
-
 	// down
 	if (keyIsDown(83)) {
 		playerLocation.z -= 2
@@ -145,29 +166,37 @@ function movePlayer() {
 	if (keyIsDown(81)) {
 		playerLocation.y += 2
 	}
-
 	// left
 	if (keyIsDown(65)) {
 		playerLocation.x -= 2
 	}
-
 	// right
 	if (keyIsDown(68)) {
 		playerLocation.x += 2
 	}
 
 	if (keyIsDown(LEFT_ARROW)) {
-		playerRotationY += 4
+		playerRotationY -= 1;
+		console.log("angle " + playerRotationY);
 	}
 
 	if (keyIsDown(RIGHT_ARROW)) {
-		playerRotationY -= 4
+		playerRotationY += 1;
+		console.log("angle " + playerRotationY);
 	}
+
+	// if (keyIsDown(UP_ARROW)) {
+	// 	playerRotationX -= 200
+	// }
+
+	// if (keyIsDown(DOWN_ARROW)) {
+	// 	playerRotationX += 200
+	// }
 }
 
 // Samples the surface distance at 4 points: pos, pos with a small x offset, pos with a small y offset and pos with a small z offset and uses the distance differences to estimate the surface normal.
 function getSurfaceNormal(pos) {
-	const epsilon = 0.0001;
+	const epsilon = 0.0000000001;
 	const centerDistance = getSurfaceDistance(pos)[0];
 	const xDistance = getSurfaceDistance(p5.Vector.add(pos, createVector(epsilon, 0, 0)))[0];
 	const yDistance = getSurfaceDistance(p5.Vector.add(pos, createVector(0, epsilon, 0)))[0];
@@ -189,9 +218,9 @@ function getSurfaceDistance(pos) {
 				smallestSphereDistance = distance;
 				closestSphere = sphere;
 			};
-		} 
+		}
 		else if (sphere.type === "plane") {
-			let distance = Math.abs(p5.Vector.dot(sphere.normal, p5.Vector.sub(pos, sphere.point)));	
+			let distance = Math.abs(p5.Vector.dot(sphere.normal, p5.Vector.sub(pos, sphere.point)));
 			if (distance < smallestSphereDistance) {
 				smallestSphereDistance = distance;
 				chosenSphere = sphere;
@@ -199,4 +228,30 @@ function getSurfaceDistance(pos) {
 		}
 	}
 	return [smallestSphereDistance, closestSphere];
+}
+
+const multiplyMatrices = (a, b) => {
+	// if (!Array.isArray(a) || !Array.isArray(b) || !a.length || !b.length) {
+	// 	throw new Error('arguments should be in 2-dimensional array format');
+	// }
+	let x = a.length,
+		z = a[0].length,
+		y = b[0].length;
+	// if (b.length !== z) {
+	// 	// XxZ & ZxY => XxY
+	// 	throw new Error('number of columns in the first matrix should be the same as the number of rows in the second');
+	// }
+	let productRow = Array.apply(null, new Array(y)).map(Number.prototype.valueOf, 0);
+	let product = new Array(x);
+	for (let p = 0; p < x; p++) {
+		product[p] = productRow.slice();
+	}
+	for (let i = 0; i < x; i++) {
+		for (let j = 0; j < y; j++) {
+			for (let k = 0; k < z; k++) {
+				product[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+	return product;
 }
