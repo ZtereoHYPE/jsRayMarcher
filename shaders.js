a6d644f729f4f1a990d088db4f77b0f5b4ed933b
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2021-08-19 00:11:12 +0200

    new: performance shader that counts no of loops

--- shaders.js
+++ shaders.js
@@ -1,10 +1,33 @@
-function normalFragmentShader(currentRayLocation) {
+function normalVoidlessShader(currentRayLocation) {
 	let normal = getSurfaceNormal(currentRayLocation);
 
 	gl_FragColor = {
 		r: (normal.x * 0.5 + 0.5) * 255,
 		g: (normal.y * 0.5 + 0.5) * 255,
 		b: (normal.z * 0.5 + 0.5) * 255
+	}
+	return gl_FragColor;
+}
+
+function normalFragmentShader(currentRayLocation, closestObjectEverDistance, closestObjectDistance) {
+	let normal = getSurfaceNormal(currentRayLocation);
+    if (closestObjectDistance > 0.1) return;
+	gl_FragColor = {
+		r: (normal.x * 0.5 + 0.5) * 255,
+		g: (normal.y * 0.5 + 0.5) * 255,
+		b: (normal.z * 0.5 + 0.5) * 255
+	}
+	return gl_FragColor;
+}
+
+// inspired from shaduwu uwu
+function cycleCounterShader(currentRayLocation, closestObjectEverDistance, closestObjectDistance, lightVector, closestObject, maxCircles) {
+    let multiplier = 255/hyperParameters.maximumLoopsPerRay;
+    let augmentedValue = maxCircles * multiplier;
+	gl_FragColor = {
+		r: augmentedValue,
+		g: augmentedValue,
+		b: augmentedValue
 	}
 	return gl_FragColor;
 }
